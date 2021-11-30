d7a44c0438851d60cce2928a643735811d3c4da5
William Cheung <william.cheung.c@gmail.com>
2021-11-30 17:24:25 +0000

    added BlogItem

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,13 @@
+<script>
+	import '../app.css';
+</script>
+
+<main>
+	<slot />
+</main>
+
+<style>
+	main {
+		padding: var(--scale-5);
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import BlogItem from '$lib/BlogItem.svelte';
+</script>
+
+<svelte:head>
+	<title>William Cheung</title>
+</svelte:head>
+
+<BlogItem />
+<BlogItem />
+
+<style>
+</style>
